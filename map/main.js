6fec46fb01576160353e616d829840a57351c123
Indy Hurt <indy.hurt@gmail.com>
2016-02-25 15:37:31 -0800

    update tracking

--- map/main.js
+++ map/main.js
@@ -148,7 +148,7 @@ map = (function () {
         anchor.target = '_blank';
         anchor.textContent = label;
         anchor.addEventListener('click', function (event) {
-            trackOutboundLink(url, 'editing_residential_buildings', type);
+            trackOutboundLink(url, 'editing_fitness_centres', type);
         }, false);
         el.appendChild(anchor);
         return el;
